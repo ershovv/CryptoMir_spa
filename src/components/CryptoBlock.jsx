7b933ccb80079a09a79a4d7743bb1d536169affc
ershovv <ershov.connect@gmail.com>
2021-12-19 23:10:01 +0300

    add image

--- src/components/CryptoBlock.jsx
+++ src/components/CryptoBlock.jsx
@@ -5,18 +5,20 @@ import {Link} from "react-router-dom";
 
 const CryptoBlock = (props) =>
 {
+	let re = /large/gi;
+
 	const dispatch = useDispatch()
 	// console.log(props.data)
 	return(
 		<Card style={{ width: '10rem' }}>
 			<Card.Body>
 				<Card.Title>{props.data.id}</Card.Title>
-				{/*<img src={props.data.image.thumb}/>*/}
+				<img src={(props.data.image).replace(re, 'small')}/>
 				<Card.Text>
 					{/*{value}*/}
 				</Card.Text>
 				<Link onClick={() => dispatch({type: "CHOOSE_COIN", name: props.data.name, id: props.data.id})}
-					  className='btn-dark'
+					  className='btn'
 					  to='/trends'>  Перейти</Link>
 			</Card.Body>
 		</Card>
